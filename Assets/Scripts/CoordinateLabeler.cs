cc6a88c50e66ee24f87c25e4f890512a1c1d6a74
Kshitijv24 <kshitijv24@gmail.com>
2022-10-09 16:35:53 +0530

    changing the color of the neighbors according to if these properties (isExplored, isPath and isWalkable

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -30,7 +30,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
-            label.enabled = false;
+            label.enabled = true;
         }
         SetLabelColor();
         ToggleLabels();
