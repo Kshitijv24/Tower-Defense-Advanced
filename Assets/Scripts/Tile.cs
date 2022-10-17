913d7fb219261d93eb5ffcc6302242c0b96ea808
Kshitijv24 <kshitijv24@gmail.com>
2022-10-17 11:10:18 +0530

    now our path finding algorithm breath first search can find the path to the destination even if we block the path with a tower, it  can find another path in real time

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
+
+    GridManager gridManager;
+    PathFinder pathFinder;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        pathFinder = FindObjectOfType<PathFinder>();
+    }
+
+    private void Start()
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (gridManager.GetNode(coordinates).isWalkable && !pathFinder.WillBlockPath(coordinates))
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+            gridManager.BlockNode(coordinates);
+        }
+    }
+}
