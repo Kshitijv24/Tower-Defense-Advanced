bbfde192716185436ff4502235f2bc419aa60301
Kshitijv24 <kshitijv24@gmail.com>
2022-10-20 14:34:14 +0530

    added build delay in our tower prefab so now our tower take a second to build itself into a full structure

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost;
+    [SerializeField] float buildDelay;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -21,5 +28,28 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            
+            yield return new WaitForSeconds(buildDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
